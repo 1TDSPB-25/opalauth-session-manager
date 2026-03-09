b76beecb56e633b956c905f550a378e59bbbc67d
eric-devops-tech <ericmaciel151@gmail.com>
2026-03-09 11:28:15 -0300

    feat: conversão de tipos e captura do campo senha

---
+++ js/login.js
@@ -0,0 +1,19 @@
+
+const botaoEnviar = document.querySelector("#btn-entrar")
+
+//botaoEnviar.addEventListener( "click" , function() {
+ //   console.log("TESTE")
+//} )
+
+function validaCampo(){
+    // Capturando os dados do campo senha:
+const campoSenha = document.getElementById("input-senha");
+let valorCampo = parseInt(campoSenha.value);
+}
+
+
+
+//Verificando se a senha é par
+console.log(valorCampo %2 == 0);
+
+botaoEnviar.addEventListener( "click" , validaCampo());
