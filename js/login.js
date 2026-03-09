72bdc1dc517209696c06e007da7d56e4f69b21bd
guilhermeoliveirafeitosa <guilhermeoliveirafeitosafacul@gmail.com>
2026-03-09 11:52:47 -0300

    feat: Simulação de Hash

--- js/login.js
+++ js/login.js
@@ -1,18 +1,29 @@
 
 const botaoEnviar = document.querySelector("#btn-entrar")
 
-
+//botaoEnviar.addEventListener("click",function() {
+//    console.log("TESTE");
+//} );
 
 botaoEnviar.addEventListener(" click " , validaCampo );
 
 
 function validaCampo(){
 
+
+//capturando os dados do campo senha.
+
 const campoSenha = document.getElementById("input-senha");
 let valorCampo = parseInt(campoSenha.value);
-   
 
+
+    
+//verificando se a senha é PAR
 console.log(valorCampo % 2 == 0);
+
+//Simulação de HASH
+const valorHash = valorCampo** 2;
+console.log(valorHash);
 
 }
 
