4c6cd170386a7fa210aa46d47ca9b6cf0d5c67df
guilhermeoliveirafeitosa <guilhermeoliveirafeitosafacul@gmail.com>
2026-03-09 11:40:56 -0300

    feat: reajustando a Conversão de tipo e captura do campo

--- js/login.js
+++ js/login.js
@@ -1,15 +1,13 @@
 
 const botaoEnviar = document.querySelector("#btn-entrar")
 
-//botaoEnviar.addEventListener("click",function() {
-//    console.log("TESTE");
-//} );
+
 
 botaoEnviar.addEventListener(" click " , validaCampo );
 
 
 function validaCampo(){
-//capturando os dados do campo senha.
+
 const campoSenha = document.getElementById("input-senha");
 let valorCampo = parseInt(campoSenha.value);
    
