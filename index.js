c6f570d6590dea2fbf281eec6fcd3144d92825da
if-Kaliel <kaliel.aquino1303@gmail.com>
2026-03-16 12:05:38 -0300

    feat: Adicionado servidor HTTP e script de inicialização.

---
+++ index.js
@@ -0,0 +1,11 @@
+//importando o objeto http para navegação do pacote node
+import http from 'node:http';
+
+//Criar um servidor HTTP com objeto http
+const server = http.createServer();
+
+server.listen(3000, ()=>{
+console.log("Servidor rodando em http://localhost:3000");
+console.log("Bem vindo!");
+});
+
