9bef63004dff161eeffd4833229bf3d6eb6bbf31
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-03-16 12:05:18 -0300

    feat: Servidor HTTP adicionado.

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//Importando o objeto HTTP para navegação do pacote node
+import http from 'node:http';
+
+//Criar um servidor HTTP com objeto http.
+const server = http.createServer();
+
+server.listen(3000, ()=>{
+    console.log("Servidor Rodando em http://localhost:3000");
+    console.log("Bem Vindo!");
+});
+
