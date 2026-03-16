0cabc776f625c51429a04e8ea6f6dffaf2a9b301
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2026-03-16 12:05:17 -0300

    feat: Criação do servidor http

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //teste
+
+// Importando o objeto http para navegação do pacote node
+import http from 'node:http';
+
+// Criar um servidor HTTP com o objeto http.
+
+const server = http.createServer();
+
+server.listen(3000, ()=>{
+    console.log("Servidor Rodando em http://localhost:3000");
+    console.log("Bem vindo!");
+});
