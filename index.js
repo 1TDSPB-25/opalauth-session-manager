34118fbd53d36ddc1ca673c4359d88b29b5b9445
Andre <asmatuda@outlook.com>
2026-03-16 12:07:08 -0300

    feat: criando e rodando server com node

---
+++ index.js
@@ -0,0 +1,12 @@
+//importando o objeto http para navegação do pacote node.
+
+import http from 'node:http';
+
+//criar um servidor http com objeto http.
+
+const server = http.createServer();
+
+server.listen(3000, () => {
+    console.log("Servidor rodando em http em http://localhost:3000 ")
+    console.log("bem vindo")
+});
