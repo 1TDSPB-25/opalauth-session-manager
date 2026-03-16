a0403574400cbd034dcb66b9af37188b71b1b81d
luizkichimoto <luizkichimoto@gmail.com>
2026-03-16 12:05:53 -0300

    feat: Criação do servidor http.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//teste
+//Importando projeto HTPP para navegação do pacote node 
+import http from 'node.https';
+// Criae um setvidor HTTP
+const server = http.createServer();
+
+server.listen(3000,()=>{
+
+    console.log("Servidor Rodando em http:localhost:3000");
+    console.log("Bem Vindo");
+
+});
